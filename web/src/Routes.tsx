a1aa1bb4713193887ea833c4e3471ae0a5710681
Aline Leroy <aline.leroy.27@gmail.com>
2022-06-25 04:21:29 +0200

    create results page, installation of Tailwind and React Icon

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -12,6 +12,7 @@ import { Router, Route } from '@redwoodjs/router'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/results-page" page={ResultsPagePage} name="resultsPage" />
       <Route notfound page={NotFoundPage} />
     </Router>
   )
---
+++ web/src/pages/ResultsPagePage/ResultsPagePage.tsx
@@ -0,0 +1,129 @@
+import { FaMapMarkerAlt, FaEuroSign } from 'react-icons/fa'
+
+import { MetaTags } from '@redwoodjs/web'
+
+const ResultsPagePage = () => {
+  return (
+    <>
+      <MetaTags title="ResultsPage" description="ResultsPage page" />
+
+      <section className="my-12 mx-12">
+        <h1 className="text-2xl mb-6">Résultats de votre recherche</h1>
+        <div className="flex justify-between">
+          <div className="flex flex-wrap">
+            <div className="w-72 border rounded-lg p-6 mr-6 mb-6">
+              <h2 className="text-xl font-bold">
+                Secrétaire médicale en hôpital
+              </h2>
+              <div className="flex items-center mt-4">
+                <FaMapMarkerAlt />
+                <p>Lille</p>
+              </div>
+              <div className="flex items-center mt-4 bg-gray-300 rounded-full p-2">
+                <p>salaire de 1000</p>
+                <FaEuroSign />
+              </div>
+              <p className="text-sm mt-4 text-gray-500">
+                At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                blanditiis praesentium voluptatum deleniti atque corrupti quos
+                dolores et quas...
+              </p>
+            </div>
+            <div className="w-72 border rounded-lg p-6 mr-6 mb-6">
+              <h2 className="text-xl font-bold">
+                Secrétaire médicale en hôpital
+              </h2>
+              <div className="flex items-center mt-4">
+                <FaMapMarkerAlt />
+                <p>Lille</p>
+              </div>
+              <div className="flex items-center mt-4 bg-gray-300 rounded-full p-2">
+                <p>salaire de 1000</p>
+                <FaEuroSign />
+              </div>
+              <p className="text-sm mt-4 text-gray-500">
+                At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                blanditiis praesentium voluptatum deleniti atque corrupti quos
+                dolores et quas...
+              </p>
+            </div>
+            <div className="w-72 border rounded-lg p-6 mr-6 mb-6">
+              <h2 className="text-xl font-bold">
+                Secrétaire médicale en hôpital
+              </h2>
+              <div className="flex items-center mt-4">
+                <FaMapMarkerAlt />
+                <p>Lille</p>
+              </div>
+              <div className="flex items-center mt-4 bg-gray-300 rounded-full p-2">
+                <p>salaire de 1000</p>
+                <FaEuroSign />
+              </div>
+              <p className="text-sm mt-4 text-gray-500">
+                At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                blanditiis praesentium voluptatum deleniti atque corrupti quos
+                dolores et quas...
+              </p>
+            </div>
+            <div className="w-72 border rounded-lg p-6 mr-6 mb-6">
+              <h2 className="text-xl font-bold">
+                Secrétaire médicale en hôpital
+              </h2>
+              <div className="flex items-center mt-4">
+                <FaMapMarkerAlt />
+                <p>Lille</p>
+              </div>
+              <div className="flex items-center mt-4 bg-gray-300 rounded-full p-2">
+                <p>salaire de 1000</p>
+                <FaEuroSign />
+              </div>
+              <p className="text-sm mt-4 text-gray-500">
+                At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                blanditiis praesentium voluptatum deleniti atque corrupti quos
+                dolores et quas...
+              </p>
+            </div>
+            <div className="w-72 border rounded-lg p-6 mr-6 mb-6">
+              <h2 className="text-xl font-bold">
+                Secrétaire médicale en hôpital
+              </h2>
+              <div className="flex items-center mt-4">
+                <FaMapMarkerAlt />
+                <p>Lille</p>
+              </div>
+              <div className="flex items-center mt-4 bg-gray-300 rounded-full p-2">
+                <p>salaire de 1000</p>
+                <FaEuroSign />
+              </div>
+              <p className="text-sm mt-4 text-gray-500">
+                At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                blanditiis praesentium voluptatum deleniti atque corrupti quos
+                dolores et quas...
+              </p>
+            </div>
+            <div className="w-72 border rounded-lg p-6 mr-6 mb-6">
+              <h2 className="text-xl font-bold">
+                Secrétaire médicale en hôpital
+              </h2>
+              <div className="flex items-center mt-4">
+                <FaMapMarkerAlt />
+                <p>Lille</p>
+              </div>
+              <div className="flex items-center mt-4 bg-gray-300 rounded-full p-2">
+                <p>salaire de 1000</p>
+                <FaEuroSign />
+              </div>
+              <p className="text-sm mt-4 text-gray-500">
+                At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                blanditiis praesentium voluptatum deleniti atque corrupti quos
+                dolores et quas...
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default ResultsPagePage
