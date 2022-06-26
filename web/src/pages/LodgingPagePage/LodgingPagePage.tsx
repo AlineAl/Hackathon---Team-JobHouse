3699ceaaf363d92a7cf35dc393c9719c89224f90
augustin <azzerty23@hotmail.fr>
2022-06-26 14:33:01 +0200

    feat: get data from db

--- web/src/pages/LodgingPagePage/LodgingPagePage.tsx
+++ web/src/pages/LodgingPagePage/LodgingPagePage.tsx
@@ -3,21 +3,56 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 
 import { Link } from '@redwoodjs/router'
 import { routes } from '@redwoodjs/router'
-import { MetaTags } from '@redwoodjs/web'
+import { MetaTags, useQuery } from '@redwoodjs/web'
 import 'swiper/css'
 import 'swiper/css/navigation'
 
 import NavBar from 'src/components/NavBar/NavBar'
 
-import image1 from '/public/img/AdobeStock_243643016.jpeg'
-import image4 from '/public/img/AdobeStock_243643016.jpeg'
-import image2 from '/public/img/AdobeStock_257275647.jpeg'
 import image6 from '/public/img/AdobeStock_292854875.jpeg'
-import image3 from '/public/img/AdobeStock_366363160.jpeg'
 import image5 from '/public/img/AdobeStock_371383825.jpeg'
 import avatar from '/public/img/avatar1.png'
 
+export const LODGINGS_QUERY = gql`
+  query LodgingsQuery {
+    lodgings {
+      id
+      Type
+      url
+      description
+      city
+    }
+  }
+`
+
+const LodgingCard = ({ data }) => {
+  return (
+    <Link to={routes.lodgingDescription()}>
+      <div className="w-72 rounded-lg">
+        <div className="relative z-0">
+          <img className="w-full bg-cover rounded-lg" src={data.url} alt="" />
+        </div>
+        <div className="relative border flex flex-col m-auto rounded-lg w-64 p-6 bg-white top-[-30px] z-40">
+          <p className="text-xl mb-2">{data.Type}</p>
+          <p className="mb-2 text-gray-500 overflow-hidden text-ellipsis h-[72px]">
+            {data.description}
+          </p>
+          <hr />
+          <div className="flex justify-between items-center mt-2">
+            <div className="flex items-center">
+              <img src={avatar} alt="" />
+              <p className="ml-2">{data.city}</p>
+            </div>
+            <p>10 Sep</p>
+          </div>
+        </div>
+      </div>
+    </Link>
+  )
+}
+
 const LodgingPagePage = () => {
+  const { data } = useQuery(LODGINGS_QUERY)
   return (
     <>
       <MetaTags title="LodgingPage" description="LodgingPage page" />
@@ -29,162 +64,10 @@ const LodgingPagePage = () => {
         </p>
         <div className="flex justify-between">
           <div className="flex flex-wrap justify-around w-[60%]">
-            <Link to={routes.lodgingDescription()}>
-              <div className="w-72 rounded-lg">
-                <div className="relative z-0">
-                  <img
-                    className="w-full bg-cover rounded-lg"
-                    src={image1}
-                    alt=""
-                  />
-                </div>
-                <div className="relative border flex flex-col m-auto rounded-lg w-64 p-6 bg-white top-[-30px] z-40">
-                  <p className="text-xl mb-2">Eiusmod tempor incididunt</p>
-                  <p className="mb-2 text-gray-500">
-                    Sed ut perspiciatis unde omnis iste natus error sit
-                    voluptate
-                  </p>
-                  <hr />
-                  <div className="flex justify-between items-center mt-2">
-                    <div className="flex items-center">
-                      <img src={avatar} alt="" />
-                      <p className="ml-2">Clara Bertoletti</p>
-                    </div>
-                    <p>10 Sep</p>
-                  </div>
-                </div>
-              </div>
-            </Link>
-            <Link to={routes.lodgingDescription()}>
-              <div className="w-72 rounded-lg">
-                <div className="relative z-0">
-                  <img
-                    className="w-full bg-cover rounded-lg"
-                    src={image6}
-                    alt=""
-                  />
-                </div>
-                <div className="relative border flex flex-col m-auto rounded-lg w-64 p-6 bg-white top-[-30px] z-40">
-                  <p className="text-xl mb-2">Eiusmod tempor incididunt</p>
-                  <p className="mb-2 text-gray-500">
-                    Sed ut perspiciatis unde omnis iste natus error sit
-                    voluptate
-                  </p>
-                  <hr />
-                  <div className="flex justify-between items-center mt-2">
-                    <div className="flex items-center">
-                      <img src={avatar} alt="" />
-                      <p className="ml-2">Clara Bertoletti</p>
-                    </div>
-                    <p>10 Sep</p>
-                  </div>
-                </div>
-              </div>
-            </Link>
-            <Link to={routes.lodgingDescription()}>
-              <div className="w-72 rounded-lg">
-                <div className="relative z-0">
-                  <img
-                    className="w-full bg-cover rounded-lg"
-                    src={image2}
-                    alt=""
-                  />
-                </div>
-                <div className="relative border flex flex-col m-auto rounded-lg w-64 p-6 bg-white top-[-30px] z-40">
-                  <p className="text-xl mb-2">Eiusmod tempor incididunt</p>
-                  <p className="mb-2 text-gray-500">
-                    Sed ut perspiciatis unde omnis iste natus error sit
-                    voluptate
-                  </p>
-                  <hr />
-                  <div className="flex justify-between items-center mt-2">
-                    <div className="flex items-center">
-                      <img src={avatar} alt="" />
-                      <p className="ml-2">Clara Bertoletti</p>
-                    </div>
-                    <p>10 Sep</p>
-                  </div>
-                </div>
-              </div>
-            </Link>
-            <Link to={routes.lodgingDescription()}>
-              <div className="w-72 rounded-lg">
-                <div className="relative z-0">
-                  <img
-                    className="w-full bg-cover rounded-lg"
-                    src={image3}
-                    alt=""
-                  />
-                </div>
-                <div className="relative border flex flex-col m-auto rounded-lg w-64 p-6 bg-white top-[-30px] z-40">
-                  <p className="text-xl mb-2">Eiusmod tempor incididunt</p>
-                  <p className="mb-2 text-gray-500">
-                    Sed ut perspiciatis unde omnis iste natus error sit
-                    voluptate
-                  </p>
-                  <hr />
-                  <div className="flex justify-between items-center mt-2">
-                    <div className="flex items-center">
-                      <img src={avatar} alt="" />
-                      <p className="ml-2">Clara Bertoletti</p>
-                    </div>
-                    <p>10 Sep</p>
-                  </div>
-                </div>
-              </div>
-            </Link>
-            <Link to={routes.lodgingDescription()}>
-              <div className="w-72 rounded-lg">
-                <div className="relative z-0">
-                  <img
-                    className="w-full bg-cover rounded-lg"
-                    src={image4}
-                    alt=""
-                  />
-                </div>
-                <div className="relative border flex flex-col m-auto rounded-lg w-64 p-6 bg-white top-[-30px] z-40">
-                  <p className="text-xl mb-2">Eiusmod tempor incididunt</p>
-                  <p className="mb-2 text-gray-500">
-                    Sed ut perspiciatis unde omnis iste natus error sit
-                    voluptate
-                  </p>
-                  <hr />
-                  <div className="flex justify-between items-center mt-2">
-                    <div className="flex items-center">
-                      <img src={avatar} alt="" />
-                      <p className="ml-2">Clara Bertoletti</p>
-                    </div>
-                    <p>10 Sep</p>
-                  </div>
-                </div>
-              </div>
-            </Link>
-            <Link to={routes.lodgingDescription()}>
-              <div className="w-72 rounded-lg">
-                <div className="relative z-0">
-                  <img
-                    className="w-full bg-cover rounded-lg"
-                    src={image5}
-                    alt=""
-                  />
-                </div>
-                <div className="relative border flex flex-col m-auto rounded-lg w-64 p-6 bg-white top-[-30px] z-40">
-                  <p className="text-xl mb-2">Eiusmod tempor incididunt</p>
-                  <p className="mb-2 text-gray-500">
-                    Sed ut perspiciatis unde omnis iste natus error sit
-                    voluptate
-                  </p>
-                  <hr />
-                  <div className="flex justify-between items-center mt-2">
-                    <div className="flex items-center">
-                      <img src={avatar} alt="" />
-                      <p className="ml-2">Clara Bertoletti</p>
-                    </div>
-                    <p>10 Sep</p>
-                  </div>
-                </div>
-              </div>
-            </Link>
+            {data &&
+              data.lodgings.map((item) => {
+                return <LodgingCard key={item.id} data={item} />
+              })}
           </div>
 
           <div className="w-[30rem] h-full border rounded-lg p-4">
--- web/src/pages/ResultsPagePage/ResultsPagePage.tsx
+++ web/src/pages/ResultsPagePage/ResultsPagePage.tsx
@@ -1,35 +1,45 @@
 import { FaMapMarkerAlt, FaEuroSign } from 'react-icons/fa'
 
 import { Link, routes } from '@redwoodjs/router'
-import { MetaTags } from '@redwoodjs/web'
+import { MetaTags, useQuery } from '@redwoodjs/web'
 
 import NavBar from 'src/components/NavBar/NavBar'
 
-const JobCard = () => {
+export const JOBS_QUERY = gql`
+  query JobsQuery {
+    jobs {
+      id
+      title
+      city
+      salary
+      description
+    }
+  }
+`
+
+const JobCard = ({ data }) => {
   return (
     <Link
       to={routes.jobPage()}
       className="w-72 border rounded-lg p-6 mr-6 mb-6"
     >
-      <h2 className="text-xl font-bold">Secrétaire médicale en hôpital</h2>
+      <h2 className="text-xl font-bold">{data.title}</h2>
       <div className="flex items-center mt-4">
         <FaMapMarkerAlt />
-        <p>Lille</p>
+        <p>{data.city}</p>
       </div>
       <div className="flex items-center mt-4 bg-gray-300 rounded-full p-2">
-        <p>salaire de 1000</p>
+        <p>{data.salary}</p>
         <FaEuroSign />
       </div>
-      <p className="text-sm mt-4 text-gray-500">
-        At vero eos et accusamus et iusto odio dignissimos ducimus qui
-        blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
-        et quas...
-      </p>
+      <p className="text-sm mt-4 text-gray-500">{data.description}</p>
     </Link>
   )
 }
 
 const ResultsPagePage = () => {
+  const { data } = useQuery(JOBS_QUERY)
+  console.log(data)
   return (
     <>
       <MetaTags title="ResultsPage" description="ResultsPage page" />
@@ -38,11 +48,10 @@ const ResultsPagePage = () => {
         <h1 className="text-2xl mb-6">Résultats de votre recherche</h1>
         <div className="flex justify-between">
           <div className="flex flex-wrap">
-            {Array(21)
-              .fill(0)
-              .map((_item, i) => (
-                <JobCard key={i} />
-              ))}
+            {data &&
+              data.jobs.map((item) => {
+                return <JobCard key={item.id} data={item} />
+              })}
           </div>
         </div>
       </section>
